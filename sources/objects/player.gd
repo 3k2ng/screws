be4898a44496130fad0db26b51f5824529d12665
emilyallerdings <skittlesanator@gmail.com>
2023-08-18 23:34:49 -0600

    jump timer

--- sources/objects/player.gd
+++ sources/objects/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+
+
 @export var movement_speed: float
 @export var jump_height : float
 @export var jump_time_to_peak : float
@@ -8,6 +10,13 @@ extends CharacterBody2D
 @onready var jump_velocity = float ((2.0 * jump_height) / jump_time_to_peak) * -1.0
 @onready var jump_gravity = float ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity = float ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+
+var on_jump_timer:float = 0
+@export var on_jump_timer_max:float 
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,8 +27,17 @@ func _physics_process(delta):
 			velocity.y += fall_gravity*delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("ui_accept"):
+		on_jump_timer = on_jump_timer_max
+		
+	if on_jump_timer > 0:
+		if is_on_floor():
+			velocity.y = jump_velocity
+			on_jump_timer = 0
+		else:
+			on_jump_timer -= delta
+	
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
