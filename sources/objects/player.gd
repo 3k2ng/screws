4a4d72fe7007a3e9f47df2022ee9f480ca852696
emilyallerdings <skittlesanator@gmail.com>
2023-08-19 01:40:50 -0600

    garbage

--- sources/objects/player.gd
+++ sources/objects/player.gd
@@ -17,6 +17,9 @@ extends CharacterBody2D
 @onready var fall_gravity = float ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 
+var wall_jump_timer = 0
+
+var prev_dir
 var can_jump:bool = false
 
 var on_jump_timer:float = 0
@@ -31,40 +34,75 @@ func _ready():
 func _physics_process(delta):
 	# Add the gravity.
 	
+	if wall_jump_timer > 0:
+		wall_jump_timer -= delta
+	
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		if wall_jump_timer <= 0:
+			velocity.x = direction * movement_speed
+	else:
+		if wall_jump_timer <= 0:
+			velocity.x = move_toward(velocity.x, 0, movement_speed)
+
+	#print (direction)
+
 	if not is_on_floor():
 		if velocity.y < 0:
 			velocity.y += jump_gravity*delta
 		else:
-			velocity.y += fall_gravity*delta
-
+			if is_on_wall() && direction != 0:
+				velocity.y += fall_gravity*delta/10
+			else:
+				velocity.y += fall_gravity*delta
+				
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		on_jump_timer = on_jump_timer_max
-	if Input.is_action_just_released("ui_accept"):
+	if Input.is_action_just_pressed("jump"):
+		if not is_on_wall():
+			on_jump_timer = on_jump_timer_max
+		else:
+			if prev_dir != 0:
+				on_jump_timer = on_jump_timer_max
+			else:
+				on_jump_timer = on_jump_timer_max
+			
+	if Input.is_action_just_released("jump"):
 		if velocity.y < 0:
 			velocity.y = 0
 		
 	if is_on_floor():
 		coyote_timer = coyote_timer_max
-	else:
+
+	if (is_on_wall_only() && direction != 0):
+		coyote_timer = coyote_timer_max
+		prev_dir = direction
+		
+	if coyote_timer > 0:
 		coyote_timer -= delta
-			
+	else:
+		prev_dir = direction
+
 	if on_jump_timer > 0:
 		if is_on_floor() || coyote_timer > 0:
-			velocity.y = jump_velocity
-			on_jump_timer = 0
-			coyote_timer = 0
+			if prev_dir != 0:
+				
+				velocity.x = -movement_speed * 2 * prev_dir 
+				print(velocity.x)
+				velocity.y = jump_velocity
+				on_jump_timer = 0
+				wall_jump_timer = 0.1
+				coyote_timer = 0
+			else:
+				velocity.y = jump_velocity
+				on_jump_timer = 0
+				coyote_timer = 0
 		else:
 			on_jump_timer -= delta
-	
-		
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * movement_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, movement_speed)
+
 		
 	move_and_slide()
