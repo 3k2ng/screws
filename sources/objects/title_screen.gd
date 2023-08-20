79a166de62a6138c8238a6a33f226c6f2759d52a
nashdashin <77025122+nashdashin@users.noreply.github.com>
2023-08-20 14:32:38 -0600

    bossmusic
    sdf

--- sources/objects/title_screen.gd
+++ sources/objects/title_screen.gd
@@ -1,7 +1,5 @@
 extends Control
 
-<<<<<<< Updated upstream
-=======
 @onready var play_music = $WinMusic
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +8,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
->>>>>>> Stashed changes
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://sources/scenes/real_level.tscn")
 	hide()
