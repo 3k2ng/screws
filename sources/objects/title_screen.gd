19f7d25cdf37837322b72a72943cbb04bad68032
nashdashin <77025122+nashdashin@users.noreply.github.com>
2023-08-20 14:45:24 -0600

    win screenf ix

--- sources/objects/title_screen.gd
+++ sources/objects/title_screen.gd
@@ -1,13 +1,20 @@
 extends Control
 
-@onready var play_music = $WinMusic
+@onready var play_music = $MainMainMenuMusic
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	play_music.play()
 	pass # Replace with function body.
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://sources/scenes/real_level.tscn")
+	get_tree().change_scene_to_file("res://sources/scenes/demo.tscn")
 	hide()
+	pass # Replace with function body.
+
+#func _on_quite_pressed() -> void:
+#	get_tree().quit()
+#	pass # Replace with function body.
