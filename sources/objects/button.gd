51cc7d2254b192d1a419288f2390ac6f7008c0a7
emilyallerdings <skittlesanator@gmail.com>
2023-08-20 08:34:14 -0600

    wirre

--- sources/objects/button.gd
+++ sources/objects/button.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var wire:TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +13,8 @@ func _process(delta):
 	pass
 
 func press():
+	if wire:
+		wire.modulate.r = 1
+		wire.modulate.g = 1
+		wire.modulate.b = 1
 	$AnimatedSprite2D.play("hit")
