5e2c9588238318438a2c3c365fa2927cc7cd2fd8
3k2ng <tung09102002@gmail.com>
2023-08-20 14:45:38 -0600

    boss music

--- sources/scenes/level_manager.gd
+++ sources/scenes/level_manager.gd
@@ -3,3 +3,5 @@ extends Node
 func start_boss(body):
 	if body.is_in_group("player"):
 		get_tree().get_first_node_in_group("boss").enable()
+	$Music.stream = preload("res://assets/Screws_Loose_Oh_No_Now_Theres_A_Boss_-_Loop.ogg")
+	$Music.play()
