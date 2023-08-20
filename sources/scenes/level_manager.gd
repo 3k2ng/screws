fbde1f9252f2cf133417745414211763e375321c
3k2ng <tung09102002@gmail.com>
2023-08-20 16:04:41 -0600

    pullll

--- sources/scenes/level_manager.gd
+++ sources/scenes/level_manager.gd
@@ -5,6 +5,8 @@ func start_boss(body):
 		get_tree().get_first_node_in_group("boss").enable()
 	$Music.stream = preload("res://assets/Screws_Loose_Oh_No_Now_Theres_A_Boss_-_Loop.ogg")
 	$Music.play()
+	GameManage.reset_life()
 
 func boss_killed():
+	await get_tree().create_timer(3.0).timeout
 	get_tree().change_scene_to_file("res://sources/scenes/win_screen.tscn")
