c598515e9f9392715554e528017a64db7c1ff6e3
nashdashin <77025122+nashdashin@users.noreply.github.com>
2023-08-19 21:54:56 -0600

    springling

--- sources/objects/screwling.gd
+++ sources/objects/screwling.gd
@@ -13,10 +13,7 @@ const BLOCK_SIZE: int = 128
 @onready var start_point: float = global_position.x
 @onready var end_point: float = global_position.x + moving_distance
 @onready var player_node = get_tree().get_first_node_in_group("player")
-@onready var player_point: float = player_node.position.x
-@onready var distance_from_player: float = (position.x - player_point)
 
-@onready var vision = $Vision
 @onready var is_moving_right = true
 
 # Called when the node enters the scene tree for the first time.
@@ -25,20 +22,52 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
+func see_player():
+	if !player_node:
+		state = PATROL
+		return
+		
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position, player_node.global_position)
+	query.exclude = [self, player_node]
+	var result = space_state.intersect_ray(query)
+	
+	if result.size() > 0:
+		state = PATROL
+		return
+		
+	var relative_position_x : float = (player_node.global_position.x - global_position.x)
+	
+	if abs(relative_position_x) > 300:
+		state = PATROL
+		return
+		
+	state = APPROACHING	
+	
+	return
+
 func _process(delta):
-	#state = PATROL	
-	if player_node:
-		$Vision.look_at(player_node.position)
+	see_player()
+	pass
 
 func _physics_process(delta):
-	if position.x >= end_point:
-		is_moving_right = false
-	elif position.x <= start_point:
-		is_moving_right = true
-	if is_moving_right:
-		velocity.x = movement_speed
-	else:
-		velocity.x = -movement_speed
+	var relative_position_x : float = (player_node.global_position.x - global_position.x)
+	if state == PATROL:
+		if position.x >= end_point:
+			is_moving_right = false
+		elif position.x <= start_point:
+			is_moving_right = true
+		if is_moving_right:
+			velocity.x = movement_speed
+		else:
+			velocity.x = -movement_speed
+	elif state == APPROACHING:
+		if(relative_position_x >= -300 and relative_position_x < 0):
+			velocity.x = -movement_speed
+		elif(relative_position_x >= 0 and relative_position_x < 300):
+			velocity.x = movement_speed
+		pass
+	print(relative_position_x)
 	if not is_on_floor():
 		velocity.y += 512 * delta
 	move_and_slide()
