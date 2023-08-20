30f6175515e084cca7f04d0780bb55d40c43d1f6
3k2ng <tung09102002@gmail.com>
2023-08-20 15:50:26 -0600

    win screen

--- sources/scenes/level_manager.gd
+++ sources/scenes/level_manager.gd
@@ -5,3 +5,6 @@ func start_boss(body):
 		get_tree().get_first_node_in_group("boss").enable()
 	$Music.stream = preload("res://assets/Screws_Loose_Oh_No_Now_Theres_A_Boss_-_Loop.ogg")
 	$Music.play()
+
+func boss_killed():
+	get_tree().change_scene_to_file("res://sources/scenes/win_screen.tscn")
