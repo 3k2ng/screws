96f17f2b8aa138c913d9d73e5a8f516a4ed171cc
3k2ng <tung09102002@gmail.com>
2023-08-20 14:54:13 -0600

    pull

--- sources/objects/title_screen.gd
+++ sources/objects/title_screen.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var play_music = $MainMainMenuMusic
+@onready var play_music = $MainMenuMusic
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	play_music.play()
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://sources/scenes/demo.tscn")
+	get_tree().change_scene_to_file("res://sources/scenes/real_level.tscn")
 	hide()
 	pass # Replace with function body.
 
