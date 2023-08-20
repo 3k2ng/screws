b2ceaaa611fe475f0e011be2d8f8fc7f438e8692
nashdashin <77025122+nashdashin@users.noreply.github.com>
2023-08-20 14:50:08 -0600

    pushsadf
    SDFSF

--- sources/objects/title_screen.gd
+++ sources/objects/title_screen.gd
@@ -1,9 +1,9 @@
 extends Control
 
-@onready var play_music = $MainMainMenuMusic
+@onready var play_music = $MainMenuMusic
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#play_music.play()
+	play_music.play()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
