447c17c257482f1fbe7c1ec62fbf9bc78cf1f0e9
3k2ng <tung09102002@gmail.com>
2023-08-31 19:48:24 -0600

    sound fix make its way to git

--- sources/scenes/level_manager.gd
+++ sources/scenes/level_manager.gd
@@ -3,11 +3,13 @@ extends Node
 func start_boss(body):
 	if body.is_in_group("player"):
 		get_tree().get_first_node_in_group("boss").enable()
-	$Music.stream = preload("res://assets/Screws_Loose_Oh_No_Now_Theres_A_Boss_-_Loop.ogg")
+	$Music.stream = preload("res://assets/Screws_Loose_Oh_No_Now_Theres_A_Boss_-_Opening (1).ogg")
 	$Music.play()
 	GameManage.reset_life()
-	
-	$Field/BossTrigger/Shape.disabled = true
+	$Field/BossTrigger/Shape.set_deferred("disabled", true)
+	await $Music.finished
+	$Music.stream = preload("res://assets/Screws_Loose_Oh_No_Now_Theres_A_Boss_-_Loop.ogg")
+	$Music.play()
 
 func boss_killed():
 	await get_tree().create_timer(3.0).timeout
